2f53999a9c81eba985af06cd157c8367e85a2eb1
cassymarie <cassy2205@gmail.com>
2021-02-11 15:32:46 -0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :charachters, only: [:show, :index] do
+    resources :sayings, only: [:show, :update]
+  end
+
+  resources :items, only: [:show, :index]
+  resources :expressions, only: [:show]
 end
