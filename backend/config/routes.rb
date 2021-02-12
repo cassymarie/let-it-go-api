de77052e418bc16550ceaea69171c6bc542e36e1
cassymarie <cassy2205@gmail.com>
2021-02-12 14:19:43 -0700

    fixed typo error with character

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :charachters, only: [:show, :index] do
+  resources :characters, only: [:show, :index] do
     resources :sayings, only: [:create, :show, :update]
   end
 
