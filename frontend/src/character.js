6bc527e8cb46be02f4d12bb0d1f4161e2b6f406f
cassymarie <cassy2205@gmail.com>
2021-02-12 15:26:20 -0700

    created Avatar class object in the Character setting, added the onClick Event for SelectAvatar, concept works (console.log)

--- frontend/src/character.js
+++ frontend/src/character.js
@@ -1,15 +1,46 @@
 const base_url = 'http://localhost:3000/'
 
+class Avatar{
+    static all = [];
+
+    constructor({id, name, title, imageUrl, knockout_phrase}){
+        this.id = id
+        this.name = name
+        this.title = title
+        this.imageUrl = imageUrl
+        this.knockout_phrase = knockout_phrase
+        this.damage = 0
+
+        Avatar.all.push(this)
+        this.element = document.createElement('div')
+
+    }
+
+    renderSelection(){
+        this.element.innerHTML = `${this.name}`
+        this.element.addEventListener('click', selectAvatar)
+        return this.element
+    }
+
+    attachToSelectionList(){
+        characters.appendChild(this.renderSelection())
+    }
+}
 
 function getCharacters(){
     fetch(`${base_url}characters`)
     .then(resp => resp.json())
     .then(json => {
         json.forEach(char => {
-            let addChar = document.createElement('div')
-            addChar.innerHTML = `${char.name}`
-            characters.appendChild(addChar)
+            const c = new Avatar(char)
+            c.attachToSelectionList()
+            // let addChar = document.createElement('div')
+            // addChar.innerHTML = `${char.name}`
+            // characters.appendChild(addChar)
         })
     })
 }
 
+function selectAvatar(e){
+    console.log(e.target)
+}
