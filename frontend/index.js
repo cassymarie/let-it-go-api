e7e62353451d1575d85397522b2daecf0fe0077c
cassymarie <cassy2205@gmail.com>
2021-02-12 14:11:34 -0700

    blocked out div with basic styles for quick view. set const selectors of container divs with identifying text.

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,12 @@
-const chars = document.querySelector('#container-characters')
+'use strict'
 
-console.log('Welcome Cassy!')
+const characters = document.querySelector('#container-charcters')
+const items = document.querySelector('#container-items')
+const saying = document.querySelector('#sayings-here')
+const my_char = document.querySelector('#selected-charcter')
+
+characters.innerHTML = 'You can select a Character from HERE'
+saying.innerHTML = "My Character's saying will go HERE"
+items.innerHTML = 'Throwing Items go in HERE'
+
+my_char.innerHTML = 'My Selected Avatar goes HERE'
